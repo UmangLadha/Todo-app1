162d5424ee61a8f8099c04fa7c4baf398050c4ee
UmangLadha <umangladha02@gmail.com>
2021-12-25 17:13:41 +0530

    bootstrap added

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className="text-uppercase">TODO App</h1>
     </div>
   );
 }
